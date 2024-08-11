40988165809a94cdb024ce4bc5d498ebf65c0d9a
juvenciofigo <juvenciofigo@gmail.com>
2024-08-11 02:03:33 +0200

    VUE_APP_API_URL

--- src/components/ui/collapsible/Collapsible.vue
+++ src/components/ui/collapsible/Collapsible.vue
@@ -1,22 +1,21 @@
 <script setup>
-import _objectSpread from "D:/PROJETOS DEV/LojaJVC/lojafront/node_modules/@babel/runtime/helpers/esm/objectSpread2.js";
-import { CollapsibleRoot, useEmitAsProps } from "radix-vue";
+    import _objectSpread from "@/../node_modules/@babel/runtime/helpers/esm/objectSpread2.js";
+    import { CollapsibleRoot, useEmitAsProps } from "radix-vue";
 
-var props = defineProps({
-  defaultOpen: { type: Boolean, required: false },
-  open: { type: Boolean, required: false },
-  disabled: { type: Boolean, required: false },
-  asChild: { type: Boolean, required: false },
-  as: { type: null, required: false },
-});
-var emits = defineEmits(["update:open"]);
+    var props = defineProps({
+        defaultOpen: { type: Boolean, required: false },
+        open: { type: Boolean, required: false },
+        disabled: { type: Boolean, required: false },
+        asChild: { type: Boolean, required: false },
+        as: { type: null, required: false },
+    });
+    var emits = defineEmits(["update:open"]);
 </script>
 
 <template>
-  <CollapsibleRoot
-    v-slot="{ open }"
-    v-bind="{ ...props, ...useEmitAsProps(emits) }"
-  >
-    <slot :open="open" />
-  </CollapsibleRoot>
+    <CollapsibleRoot
+        v-slot="{ open }"
+        v-bind="{ ...props, ...useEmitAsProps(emits) }">
+        <slot :open="open" />
+    </CollapsibleRoot>
 </template>
--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,3 @@
 export default {
-    // apiURL: process.env.VUE_APP_API_URL,
-    apiURL: "https://lojab-deployed.onrender.com",
+    apiURL: process.env.VUE_APP_API_URL,
 };
