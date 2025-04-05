7590c5f55f2deb4e2322ec82b13b07ad9fb8c35e
juvenciofigo <juvenciofigo@gmail.com>
2025-04-05 09:23:52 +0200

    Update DashboardView.vue

--- src/resources/Dashboard/_components/DashboardView.vue
+++ src/resources/Dashboard/_components/DashboardView.vue
@@ -5,34 +5,54 @@
             <template #left>
                 <LogoPart class="mb-2" />
 
-                <div class="md:hidden">
-                    <el-button
-                        size="small"
-                        @click="isCollapse = !isCollapse">
-                        <el-icon> Menu </el-icon>
-                    </el-button>
-                </div>
+                <el-dropdown
+                    size="large"
+                    type="primary"
+                    :persistent="false"
+                    trigger="click"
+                    :hide-on-click="true"
+                    role="navigation"
+                    popper-class="el-menu-vertical-demo"
+                    class="md:hidden el-menu-vertical-demo">
+                    <span class="el-dropdown-link flex flex-row items-center ">
+                        Menu
+                        <el-icon class="el-icon--right ">
+                            <Grid/>
+                        </el-icon>
+                    </span>
+                    <template #dropdown>
+                        <el-dropdown-menu>
+                            <el-dropdown-item
+                                v-for="(item, index) in items"
+                                :key="index">
+                                <template #default>
+                                    <router-link
+                                        :to="{ name: item.link }"
+                                        exact-active-class="text-blue-400">
+                                        <el-icon><component :is="item.icon" /></el-icon>
+                                        <span class="whitespace-nowrap">{{ item.name }} </span>
+                                    </router-link>
+                                </template>
+                            </el-dropdown-item>
+                        </el-dropdown-menu>
+                    </template>
+                </el-dropdown>
             </template>
 
             <!-- direita -->
             <template #right>
-                <template
-                    v-for="(route, index) in breadcrumb"
-                    :key="index">
-                    <span class="capitalize"> {{ route }} > </span>
-                </template>
-
                 <el-breadcrumb separator="/">
                     <el-breadcrumb-item
                         v-for="(route, index) in breadcrumb"
-                        :key="index">
-                        <a href="/dashboard">{{ route }}</a>
+                        :key="index"
+                        :to="route.route">
+                        <span>{{ route.name }}</span>
                     </el-breadcrumb-item>
                 </el-breadcrumb>
             </template>
         </HeaderTop>
 
-        <div class="flex flex-row">
+        <div class="flex flex-row flex-1">
             <div class="flex flex-col">
                 <span class="hidden md:block">
                     <el-button
@@ -60,37 +80,20 @@
                     </el-menu>
                 </span>
             </div>
-            <div :class="[isCollapse ? 'hidden' : '', `absolute menu-mobile z-50`]">
-                <el-menu
-                    class="el-menu-vertical-demo md:hidden"
-                    :ellipsis="false">
-                    <el-menu-item
-                        v-for="(item, index) in items"
-                        :key="index"
-                        :index="String(index)">
-                        <router-link
-                            :to="{ name: item.link }"
-                            exact-active-class="text-blue-400">
-                            <el-icon><component :is="item.icon" /></el-icon>
-                            <span
-                                @click="() => (isCollapse = false)"
-                                :class="['whitespace-nowrap']"
-                                >{{ item.name }}</span
-                            >
-                        </router-link>
-                    </el-menu-item>
-                </el-menu>
-            </div>
             <router-view class=""></router-view>
         </div>
+
+        <FoooterComp />
     </div>
 </template>
 
 <script setup>
     import { computed, ref } from "vue";
-    import { useRouter } from "vue-router";
+    import { useRoute } from "vue-router";
     import LogoPart from "@/components/partials/LogoPart.vue";
+    import FoooterComp from "@/components/partials/FoooterComp.vue";
     import { ShoppingCart, User, Goods, ShoppingBag, TakeawayBox, Grid, ArrowLeft } from "@element-plus/icons-vue";
+    const route = useRoute();
 
     const items = [
         { name: "Dahsboard", link: "admin-home", icon: Grid },
@@ -102,12 +105,22 @@
     ];
 
     const isCollapse = ref(false);
-    const router = useRouter();
     const breadcrumb = computed(() => {
-        const paths = router.currentRoute.value.path.split("/").filter((path) => path !== "");
-        console.log("corente", router.currentRoute.value);
+        const seen = new Set();
 
-        return paths.slice(0, 3);
+        return route.matched
+            .map((r) => {
+                const el = r.path.split("/");
+                return { name: el[el.length - 1].toUpperCase(), route: r.path };
+            })
+            .filter((item) => {
+                // Se já vimos este nome, filtra fora
+                if (seen.has(item.name)) return false;
+
+                seen.add(item.name);
+
+                return true;
+            });
     });
 </script>
 
