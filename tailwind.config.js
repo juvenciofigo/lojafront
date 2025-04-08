1371bcf30f696c3919113d3098691b75f08b6a74
juvenciofigo <juvenciofigo@gmail.com>
2025-04-08 07:44:50 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
-// tailwind.config.js
 module.exports = {
-    content: ["./pages/**/*.{js,jsx,vue}", "./components/**/*.{js,jsx,vue}", "./app/**/*.{js,jsx,vue}", "./src/**/*.{js,jsx,vue}", "./src/**/*.{vue,js,ts,jsx,tsx}", "./public/**/*.html"],
+    content: [
+      "./pages/**/*.{js,jsx,vue}",
+      "./components/**/*.{js,jsx,vue}",
+      "./app/**/*.{js,jsx,vue}",
+      "./src/**/*.{js,jsx,vue}",
+      "./src/**/*.{vue,js,ts,jsx,tsx}",
+      "./public/**/*.html"
+    ],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          primary: '#FF5733',
+          secondary: '#33FF57'
+        },
+        spacing: {
+          128: '32rem'
+        }
+      }
     },
-    plugins: [],
+    plugins: [
+      require('@tailwindcss/forms'),
+      require('@tailwindcss/typography')
+    ]
   }
   
