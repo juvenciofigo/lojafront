7b2985a98d0fc5cd6dcf5458010cd43b041d47bb
juvenciofigo <juvenciofigo@gmail.com>
2025-04-30 22:56:46 +0300

    Upadate banners

--- src/resources/Store/_components/Store/_components/banners/BannerComp.vue
+++ src/resources/Store/_components/Store/_components/banners/BannerComp.vue
@@ -8,27 +8,32 @@
                 v-for="(item, index) in items"
                 :key="index"
                 class="h-auto">
-                <el-image
-                    :src="item.url"
-                    :alt="`Descrição do Banner ${index + 1}`"
-                    lazy>
-                    <template #placeholder>
-                        <el-skeleton-item
-                            variant="image"
-                            style="width: 100%; height: 100%" />
-                    </template>
-                    <template #error>
-                        <div class="w-full h-48 bg-gray-200 flex items-center justify-center text-red-500">
-                            <i class="el-icon-picture-outline"></i>
-                            <span>Imagem não pôde ser carregada</span>
-                        </div>
-                    </template>
-                </el-image>
+                <router-link :to="{ name: 'product-details', params: { id: `${item.productId}` } }">
+                    <el-image
+                        :src="item.url"
+                        :alt="`Descrição do Banner ${index + 1}`"
+                        lazy>
+                        <template #placeholder>
+                            <el-skeleton-item
+                                variant="image"
+                                style="width: 100%; height: 100%" />
+                        </template>
+                        <template #error>
+                            <div class="w-full h-48 bg-gray-200 flex items-center justify-center text-red-500">
+                                <i class="el-icon-picture-outline"></i>
+                                <span>Imagem não pôde ser carregada</span>
+                            </div>
+                        </template>
+                    </el-image>
+                </router-link>
             </el-carousel-item>
         </el-carousel>
     </div>
 </template>
 
 <script setup>
-    const items = [{ url: "images/baners/banner1.webp" }, { url: "images/baners/banner2.webp" }];
+    const items = [
+        { url: "images/baners/Banner-calca-treino.png", productId: "68115fe02b85d6a0fa26aa44" },
+        { url: "images/baners/Banner-calca-treino.png", productId: "68115fe02b85d6a0fa26aa44" },
+    ];
 </script>
